0101d3c9a19f16da4d630879f55056c4d716a66a
Thiery Laverdure <thiery@spacestud.io>
2025-11-07 08:48:32 -0500

    Refactor integration test setup for ApiClient

--- tests/Integration/ApiClientTest.php
+++ tests/Integration/ApiClientTest.php
@@ -7,41 +7,26 @@ namespace Litebase\Tests\Integration;
 use Litebase\ApiClient;
 use Litebase\Configuration;
 
-beforeAll(function () {
+$configuration = new Configuration;
+
+$configuration
+    ->setHost('localhost')
+    ->setPort('8888')
+    ->setUsername('root')
+    ->setPassword('password');
+
+$client = new ApiClient($configuration);
+
+beforeAll(function () use ($client) {
     exec('docker compose -f ./tests/docker-compose.test.yml up -d');
 
     // Give the container a moment to initialize
     sleep(2);
 
-    // Wait for the API to be healthy. Poll the configured host/port until we receive a response
-    $url = 'http://localhost:8888/';
-    $timeoutSeconds = 60;
-    $start = time();
+    $response = $client->clusterStatus()->listClusterStatuses();
 
-    while (true) {
-        $context = stream_context_create([
-            'http' => [
-                'method' => 'GET',
-                'timeout' => 2,
-                'ignore_errors' => true,
-            ],
-        ]);
-
-        $success = @file_get_contents($url, false, $context);
-
-        if ($success !== false) {
-            // API is healthy
-            break;
-        }
-
-        if (time() - $start > $timeoutSeconds) {
-            // Tear down in case of failure and raise an error
-            exec('docker compose -f ./tests/docker-compose.test.yml down -v');
-            throw new \RuntimeException("Timed out waiting for the API to become healthy at {$url}");
-        }
-
-        // Back off before retrying
-        sleep(2);
+    if ($response->getStatus() !== 'success') {
+        throw new \RuntimeException('Failed to connect to Litebase server for integration tests.');
     }
 });
 
@@ -51,16 +36,6 @@ afterAll(function () {
     exec('rm -rf ./tests/.litebase');
 });
 
-describe('ApiClient', function () {
-    $configuration = new Configuration;
-
-    $configuration
-        ->setHost('localhost')
-        ->setPort('8888')
-        ->setUsername('root')
-        ->setPassword('password');
-
-    $client = new ApiClient($configuration);
-
+describe('ApiClient', function () use ($client) {
     ApiClientTestRunner::run($client);
 });
