80b57a31079014bdbedadf6040a6dff72ab8e187
Thiery Laverdure <thiery@spacestud.io>
2026-01-06 11:25:56 -0500

    Update test runner

--- tests/Integration/LitebaseContainer.php
+++ tests/Integration/LitebaseContainer.php
@@ -13,16 +13,16 @@ class LitebaseContainer
 
         $startCommand = 'docker run -d --rm --name litebase-test -p 8888:8888 \\
             -e LITEBASE_CLUSTER_ID=cluster-1 \\
-            -e LITEBASE_DATA_PATH=/tmp/data \\
             -e LITEBASE_ENCRYPTION_KEY=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa \\
             -e LITEBASE_ENV=testing \\
             -e LITEBASE_PORT=8888 \\
             -e LITEBASE_ROOT_USERNAME=root \\
             -e LITEBASE_ROOT_PASSWORD=password \\
+            -e LITEBASE_STORAGE_LOCAL_PATH=/tmp/data \\
             -e LITEBASE_STORAGE_NETWORK_PATH=/tmp/data/_network \\
             -e LITEBASE_STORAGE_TMP_PATH=/tmp \\
             -e LITEBASE_STORAGE_OBJECT_MODE=local \\
-            litebase/litebase start';
+            litebase/litebase:latest start';
 
         shell_exec($startCommand);
 
